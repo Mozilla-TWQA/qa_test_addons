f555f53ee9dddda4ae174b1e1f6c2191a744a62e
Shing Lyu <shing.lyu@gmail.com>
2015-10-08 16:29:44 +0800

    Try to make generate multiple addon script compatiable with OSX

--- generate_multiple_addons.sh
+++ generate_multiple_addons.sh
@@ -3,7 +3,7 @@ mkdir multiple-addon-example
 for i in $(seq 1 10)
 do
   cp -r certified-app-example multiple-addon-example/addon-$i
-  sed -i "s/Certified App/Test Addon $i/g" multiple-addon-example/addon-$i/*.json
-  sed -i "s/Certified App/Test Addon $i/g" multiple-addon-example/addon-$i/*.webapp
-  sed -i "s/ADDON_ID/$i/g" multiple-addon-example/addon-$i/*.js
+  sed -i -e "s/Certified App/Test Addon $i/g" multiple-addon-example/addon-$i/*.json
+  sed -i -e "s/Certified App/Test Addon $i/g" multiple-addon-example/addon-$i/*.webapp
+  sed -i -e "s/ADDON_ID/$i/g" multiple-addon-example/addon-$i/*.js
 done
